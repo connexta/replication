65f8bbb48cff669535ff547f6bb231c5a6f1df57
Peter Huffer <peter.huffer@connexta.com>
2019-03-12 12:14:28 -0700

    Improve not found routes

--- ui/src/main/webapp/app.js
+++ ui/src/main/webapp/app.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import SitesPage from './components/sites/SitesPage'
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch, Link } from 'react-router-dom'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import client from './client'
 import { ApolloProvider } from 'react-apollo'
 import { SnackbarProvider } from 'notistack'
+import { Grid, Typography } from '@material-ui/core'
 
 const theme = createMuiTheme({
   // see: https://material-ui.com/style/typography/#migration-to-typography-v2
@@ -14,6 +15,29 @@ const theme = createMuiTheme({
     useNextVariants: true,
   },
 })
+
+const NotFoundPage = () => {
+  return (
+    <div style={{ flexGrow: 1, width: '90%', margin: '65px auto' }}>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <Typography variant='h3'>Oops!</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='body1'>
+            The page you are looking for cannot be found. Try using the menu to
+            navigate the site.
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant='body1'>
+          Or click <Link to='/'>here</Link> to return home.
+        </Typography>
+      </Grid>
+    </div>
+  )
+}
 
 const App = () => {
   return (
@@ -29,7 +53,7 @@ const App = () => {
                   <Route exact path='/nodes' component={SitesPage} />
                   <Route
                     render={function() {
-                      return <p>Not Found</p>
+                      return <NotFoundPage />
                     }}
                   />
                 </Switch>
