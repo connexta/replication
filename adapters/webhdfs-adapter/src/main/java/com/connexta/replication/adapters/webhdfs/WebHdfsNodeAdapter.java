b9991abceb26473f431b57bb0c60ff33b12f0cba
Chris Lange <chris.lange@connexta.com>
2020-04-30 15:32:29 -0400

    implemented createResource method with MultipartEntityBuilder

--- adapters/webhdfs-adapter/src/main/java/com/connexta/replication/adapters/webhdfs/WebHdfsNodeAdapter.java
+++ adapters/webhdfs-adapter/src/main/java/com/connexta/replication/adapters/webhdfs/WebHdfsNodeAdapter.java
@@ -1,5 +1,25 @@
+/**
+ * Copyright (c) Connexta
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
 package com.connexta.replication.adapters.webhdfs;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.codice.ditto.replication.api.NodeAdapter;
 import org.codice.ditto.replication.api.data.CreateRequest;
 import org.codice.ditto.replication.api.data.CreateStorageRequest;
@@ -7,14 +27,27 @@ import org.codice.ditto.replication.api.data.DeleteRequest;
 import org.codice.ditto.replication.api.data.Metadata;
 import org.codice.ditto.replication.api.data.QueryRequest;
 import org.codice.ditto.replication.api.data.QueryResponse;
+import org.codice.ditto.replication.api.data.Resource;
 import org.codice.ditto.replication.api.data.ResourceRequest;
 import org.codice.ditto.replication.api.data.ResourceResponse;
 import org.codice.ditto.replication.api.data.UpdateRequest;
 import org.codice.ditto.replication.api.data.UpdateStorageRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 
 public class WebHdfsNodeAdapter implements NodeAdapter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebHdfsNodeAdapter.class);
+
+    private final URL webHdfsUrl;
+
+    public WebHdfsNodeAdapter(URL webHdfsUrl) {
+        this.webHdfsUrl = webHdfsUrl;
+    }
 
     @Override
     public boolean isAvailable() {
@@ -23,7 +56,7 @@ public class WebHdfsNodeAdapter implements NodeAdapter {
 
     @Override
     public String getSystemName() {
-        return null;
+        return "webHDFS";
     }
 
     @Override
@@ -58,7 +91,32 @@ public class WebHdfsNodeAdapter implements NodeAdapter {
 
     @Override
     public boolean createResource(CreateStorageRequest createStorageRequest) {
-        return false;
+        List<Resource> resources = createStorageRequest.getResources();
+        try(CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(webHdfsUrl.toString());
+
+            Resource resource = resources.get(0);
+
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.addBinaryBody(
+                    "file",
+                    resource.getInputStream(),
+                    ContentType.create(resource.getMimeType()),
+                    "file.ext"
+            );
+            HttpEntity multipart = builder.build();
+            httpPost.setEntity(multipart);
+
+            CloseableHttpResponse response = client.execute(httpPost);
+            int status = response.getStatusLine().getStatusCode();
+            if (status >= 300) {
+                LOGGER.debug("Failed to replicate.  Status code: {}", status);
+                return false;
+            }
+        } catch (IOException e) {
+            LOGGER.debug("Failed to create resource on remote system", e);
+        }
+        return true;
     }
 
     @Override
